1522f400b35c78fd980404f7f100aaca9ecae21e
dekokun <dekokun@hatena.ne.jp>
2020-08-01 14:46:41 +0900

    add normal test

--- json2env_test.go
+++ json2env_test.go
@@ -1,1 +1,15 @@
 package json2env
+
+import (
+	"bytes"
+	"context"
+	"testing"
+)
+
+func TestNormal(t *testing.T) {
+	stdin := bytes.NewBufferString("{}")
+	err := Run(context.Background(), []string{}, &bytes.Buffer{}, &bytes.Buffer{}, stdin)
+	if err != nil {
+		t.Fatalf("failed test %#v", err)
+	}
+}
